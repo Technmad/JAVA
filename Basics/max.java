363b5fdb7464a56c26b0094b5407fc4f16e23ccb
anurag <anuragpathakoffl@gmail.com>
2023-07-25 00:54:54 +0530

    Max of two no.

---
+++ Basics/max.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class max {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        System.out.println("num1");
+        double num1 = sc.nextDouble();
+        System.out.println("num2");
+        double num2 = sc.nextDouble();
+        if (num1 > num2) {
+            System.out.println("largest of two number is = " + num1);
+        } else {
+            System.out.println("largest of two number is = " + num2);
+        }
+    }
+}
