ed4f61403fa75a6f3201943f50c1d822fcb93f1c
anurag <anuragpathakoffl@gmail.com>
2023-07-25 00:52:28 +0530

    java program to add two no

---
+++ Basics/sum.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter the number 1 & 2 :");
+        int num1= input.nextInt();
+        int num2= input.nextInt();
+        int sum = num1 + num2;
+        System.out.println("com.FP.sum =" + sum);
+
+    }
+}
