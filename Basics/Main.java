d0145841ae8fce4a8d87e3c7602c6f2754b1e3bc
anurag <anuragpathakoffl@gmail.com>
2023-07-25 00:43:53 +0530

    java

---
+++ Basics/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println( input.nextLine());
+        System.out.println("Anurag Pathak");
+
+
+    }
+}
