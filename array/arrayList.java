5e82323aa5babe82fb76df2ee94d3ee42ec303d1
anurag <anuragpathakoffl@gmail.com>
2023-08-06 23:13:32 +0530

    arrayList

---
+++ array/arrayList.java
@@ -0,0 +1,13 @@
+package array;
+
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        // using wrapper classes for primitive data type
+        ArrayList<Integer> list=new ArrayList<>(10);
+
+
+
+    }
+}
