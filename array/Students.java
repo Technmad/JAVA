b5e3ffe7906b5b5d29f112646d7a09a3ef1d70cb
anurag <anuragpathakoffl@gmail.com>
2023-08-07 23:24:28 +0530

    ls of students

---
+++ array/Students.java
@@ -0,0 +1,14 @@
+package array;
+
+// Creating a Student class
+// Used in misc.test2
+
+public class Students {
+  public  int roll_no;
+  public  String name;
+
+ public   Students(int roll_no, String name){
+        this.name = name;
+        this.roll_no=roll_no;
+    }
+}
