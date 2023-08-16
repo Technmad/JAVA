b4df2a2177ea66dec6e15aeab50a50bfeeba235a
anurag <anuragpathakoffl@gmail.com>
2023-08-16 23:56:08 +0530

    array sum

--- Misc/p1.java
+++ Misc/p1.java
@@ -2,20 +2,20 @@
 
 import java.util.Scanner;
 
-public class p1{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int h=sc.nextInt();
-        int sum=0;
-        int[] arr=new int[n];
+public class p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int h = sc.nextInt();
+        int sum = 0;
+        int[] arr = new int[n];
 
-        for(int i=0; i<n; ++i){
-            arr[i]=sc.nextInt();
-            if(arr[i]>h){
-                sum = sum+2;
-            }else{
-                sum=sum+1;
+        for (int i = 0; i < n; ++i) {
+            arr[i] = sc.nextInt();
+            if (arr[i] > h) {
+                sum = sum + 2;
+            } else {
+                sum = sum + 1;
             }
         }
         System.out.println(sum);
